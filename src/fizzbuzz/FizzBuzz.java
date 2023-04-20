47790c3b464c225386c9123d6eb4b83f1d564414
Corentin <corentin.ludwig@outlook.fr>
2023-04-20 16:58:36 +0200

    test 5

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -1,11 +1,16 @@
 package fizzbuzz;
 
 public class FizzBuzz {
+	private static final int[] numSpeciaux = {3,5};
+	private static final String[] trad = {"fizz","buzz"};
 
-	public String fizzBuzz(int i) {
-		if(i==3) 
-			return "fizz";
-		return Integer.toString(i);
+
+	public String fizzBuzz(int num) {
+		for (int i = 0; i<numSpeciaux.length; i++) {
+			if(num == numSpeciaux[i]) 
+				return trad[i];
+		}
+		return Integer.toString(num);
 	}
 
 }
